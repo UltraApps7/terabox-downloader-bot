c74547a4914db09c459a4820ebabb3f08ac444a3
UltraApps7 <ultraapk89057@gmail.com>
2025-02-08 01:16:36 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,19 +5,19 @@ BOT_TOKEN = "5970787431:AAHDlpbbnBhGyyKIaKdFWOpmJAYc6kte18M"  # bot token
 
 
 # REDIS
-HOST = "localhost"  # redis host uri
-PORT = 6379  # redis port
-PASSWORD = ""  # redis password
+HOST = "redis-16688.c266.us-east-1-3.ec2.redns.redis-cloud.com:16688"  # redis host uri
+PORT = 16688  # redis port
+PASSWORD = "6S9NNMhR20Tz9KzKJPfe819OaexvjmL4"  # redis password
 
 PRIVATE_CHAT_ID = -1002251180934  # CHAT WHERE YOU WANT TO STORE VIDEOS
 # COOKIE FOR AUTHENTICATION (get from chrome dev tools) ex: "PANWEB=1; csrfToken=;
 COOKIE = "ndus=Yz1EnixteHuiv0z8wUQw_EhAs-DGVSLRCWC1z57o;csrfToken=gWNw3HdEIQawTDrgNGjx7Fpy;browserid=OMyocqxAdccyJuOYBHkkN7lrp-_Rd1637R1LbYWnmrM6r_99AdPCZ1KssKY=;_ga=GA1.1.891203234.1738175388;_ga_06ZNKL8C2E=GS1.1.1738956376.4.0.1738956384.52.0.0;__bid_n=194b3c871e666a4d724207;lang=en;ndut_fmt=F0DEAC76AABA8C8CF87FDEBECFB3528B85C75DF3E213F80C98C2AE6C78DA6088;TSID=b4FivTcJrjBSzgXG8R0O8la4Yg7MmWha"
-ADMINS = [1317173146]
+ADMINS = [1865273492]
 
 
-BOT_USERNAME = "teraboxdown_bot"
+BOT_USERNAME = "autogiveaway_bot"
 
 # Force user to join this channel. (make sure you have promoted the bot on this chat.)
 FORCE_LINK = "@RoldexVerse"
 
-PUBLIC_EARN_API = ""  # https://publicearn.com/api
+PUBLIC_EARN_API = "8700018bffc724d4b39f7be1d60d680fbc2ab718"  # https://publicearn.com/api
